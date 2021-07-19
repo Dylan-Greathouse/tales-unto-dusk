ddb042a2a66452e77b9152390563cd2da3d8fa2a
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-19 16:47:12 -0700

    added common folder and two files including two functions. findById and died

---
+++ common/died.js
@@ -0,0 +1,5 @@
+function died(mercenary) {
+    return mercenary.hp <= 0;
+}
+
+export default died;
--- map/map.js
+++ map/map.js
@@ -1,18 +1,9 @@
 // import the quest data
-import quests from '../data/quest-data';
+import bounties from '../data/bounty-data.js';
+import { getMercenary } from '../data/storage-data.js';
+import died from '../common/died.js';
 
-const questList = document.getElementById('quest-list');
 
-for(let quest of quests) {
-    const href = `../quest/?questId=${quest.id}`;
-
-    const questLink = document.createElement('a');
-
-    questLink.href = questHref;
-    questLink.textContent = quest.title;
-
-    questList.appendChild(questLink);
-}
 // generate an anchor tag for each test
 // use the quest's ID as the query parameter
 //
