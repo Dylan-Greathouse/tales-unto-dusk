964f212cfa788afe498754e9d97f8853788fc480
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-19 17:03:46 -0700

    added to common folder a load-profile file

---
+++ common/load-profile.js
@@ -0,0 +1,27 @@
+import { getMercenary } from '../data/storage-data.js';
+import died from './died.js';
+
+function loadProfile() {
+    const name = document.getElementById('name');
+    const job = document.getElementById('job');
+    const hp = document.getElementById('hp');
+    const gold = document.getElementById('gold');
+
+    const mercenary = getMercenary();
+
+    if (!mercenary) {
+        window.location = './';
+    }
+
+    name.textContent = mercenary.name;
+    job.src = '' + mercenary.job + '.png';
+    gold.textContent = mercenary.gold;
+
+    if (died(mercenary)) {
+        hp.textContent = 'GAME OVER';
+    } else {
+        hp.textContent = mercenary.hp;
+    }
+}
+
+export default loadProfile;
--- map/map.js
+++ map/map.js
@@ -2,6 +2,7 @@
 import bounties from '../data/bounty-data.js';
 import { getMercenary } from '../data/storage-data.js';
 import died from '../common/died.js';
+import loadProfile from '../common/load-profile.js';
 
 
 // generate an anchor tag for each test
